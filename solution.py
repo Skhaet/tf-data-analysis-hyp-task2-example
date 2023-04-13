30119835df91492a577fc95a8447c20c9a0b59b6
Skhaet <62569156+Skhaet@users.noreply.github.com>
2023-04-13 21:27:07 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import random 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 270620880 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
