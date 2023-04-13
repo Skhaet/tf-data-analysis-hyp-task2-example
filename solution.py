346eed1af204a6d2c99956f2ba1fe536d977d8e2
Skhaet <62569156+Skhaet@users.noreply.github.com>
2023-04-13 22:14:03 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import numpy as np
-import random 
 
+import scipy.stats as st
 chat_id = 270620880 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return bool(random.getrandbits(1)) # Ваш ответ, True или False
+    res = st.anderson_ksamp([x, y])
+    a = 0.06
+    ret = True
+    #ret = (res.pvalue <= a)
+    return  res.pvalue <= 0.06 # Ваш ответ, True или False
