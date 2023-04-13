a7170f1ee048f41c1cc57f40889f90e9736ce6b7
Skhaet <62569156+Skhaet@users.noreply.github.com>
2023-04-13 21:25:31 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-
+import random 
 
 chat_id = 123456 # Ваш chat ID, не меняйте название переменной
 
@@ -8,4 +8,4 @@ def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return True # Ваш ответ, True или False
+    return bool(random.getrandbits(1)) # Ваш ответ, True или False
